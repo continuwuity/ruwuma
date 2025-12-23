864079bf0e7369c20fa2f4fe83317866f41ebd87
timedout <git@nexy7574.co.uk>
2025-12-23 18:12:45 +0000

    fix: Federation invite endpoint now uses full-sized PDUs
    Also removed v1 invite code because it's unused

--- crates/ruma-federation-api/src/membership/create_invite.rs
+++ crates/ruma-federation-api/src/membership/create_invite.rs
@@ -1,6 +1,4 @@
 //! `PUT /_matrix/federation/*/invite/{roomId}/{eventId}`
 //!
 //! Invites a remote user to a room.
-
-pub mod v1;
 pub mod v2;
--- crates/ruma-federation-api/src/membership/create_invite/v2.rs
+++ crates/ruma-federation-api/src/membership/create_invite/v2.rs
@@ -10,7 +10,7 @@ use ruma_common::{
     serde::Raw,
     OwnedEventId, OwnedRoomId, RoomVersionId,
 };
-use ruma_events::AnyStrippedStateEvent;
+use ruma_events::AnyStateEvent;
 use serde_json::value::RawValue as RawJsonValue;
 
 const METADATA: Metadata = metadata! {
@@ -39,8 +39,10 @@ pub struct Request {
     /// The invite event which needs to be signed.
     pub event: Box<RawJsonValue>,
 
-    /// An optional list of simplified events to help the receiver of the invite identify the room.
-    pub invite_room_state: Vec<Raw<AnyStrippedStateEvent>>,
+    /// An optional list of events to help the receiver of the invite identify the room.
+    /// 
+    /// NOTE: As of v1.16, this endpoint now requires full room-formatted PDUs, not stripped state events.
+    pub invite_room_state: Vec<Raw<AnyStateEvent>>,
 
     /// An optional list of servers the invited homeserver should attempt to join or leave via,
     /// according to [MSC4125](https://github.com/matrix-org/matrix-spec-proposals/pull/4125).
@@ -66,7 +68,7 @@ impl Request {
         event_id: OwnedEventId,
         room_version: RoomVersionId,
         event: Box<RawJsonValue>,
-        invite_room_state: Vec<Raw<AnyStrippedStateEvent>>,
+        invite_room_state: Vec<Raw<AnyStateEvent>>,
     ) -> Self {
         Self {
             room_id,
